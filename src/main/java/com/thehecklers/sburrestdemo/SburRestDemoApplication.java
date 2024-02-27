65f838a9b1bfb98ceeee97c6b1248a4d3c188f29
chp320 <chp320@naver.com>
2024-02-27 23:48:49 +0900

    GET 메서드 처리

--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -2,6 +2,13 @@ package com.thehecklers.sburrestdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class SburRestDemoApplication {
@@ -10,4 +17,50 @@ public class SburRestDemoApplication {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
+		));
+	}
+
+	@RequestMapping(value = "/coffess", method = RequestMethod.GET)
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+}
+
+class Coffee {
+	private final String id;		// final 로 선언되어 한 번 할당 이후 절대 수정 불가
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	// id 매개변수 미전달 시, 고유 식별자인 id를 기본 제공
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
